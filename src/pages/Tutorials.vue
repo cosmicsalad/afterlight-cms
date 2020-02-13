15caa8a0252bdcf84b8f2e4c87efc0bf06c313b4
Andrew <andrewjfilip@gmail.com>
2020-02-12 21:02:28 -0800

    Update Tutorials.vue

--- src/pages/Tutorials.vue
+++ src/pages/Tutorials.vue
@@ -2,8 +2,8 @@
   <Layout>
 
     <div class="container">
-      <div class="journal-hero">
-        <h1 class="journal-header">
+      <div class="tutorial-hero">
+        <h1 class="tutorial-header">
           some sort of heading here.
         </h1>
       </div>
@@ -13,11 +13,11 @@
       :to="item.node.path"
       v-for="item in $page.posts.edges" 
       :key="item.node.id"
-      class="journal-post"
+      class="tutorial-post"
     >
-      <div class="container journal">
-        <h2 class="journal-title">{{ item.node.title }}</h2>
-        <p class="journal-excerpt">{{ item.node.excerpt }}</p>
+      <div class="container tutorial">
+        <h2 class="tutorial-title">{{ item.node.title }}</h2>
+        <p class="tutorial-excerpt">{{ item.node.excerpt }}</p>
       </div>
     </g-link>
       
@@ -45,56 +45,56 @@ export default {
 </script>
 
 <style scoped>
-.container.journal {
+.container.tutorial {
   max-width: 720px;
 }
-.journal-hero {
+.tutorial-hero {
   padding: 4rem 0;
   text-align: center;
   color: var(--color-base-1);
 }
-.journal-header {
+.tutorial-header {
   font-size: 3rem;
   font-weight: 700;
   padding: 0;
   margin: 0;
 }
-.journal-post {
+.tutorial-post {
   display: block;
   padding: 2rem 0;
   text-decoration: none;
   transition: background 0.5s ease;
 }
-.journal-post > * {
+.tutorial-post > * {
   transition: transform 0.5s ease;
 }
-.journal-post:hover {
+.tutorial-post:hover {
   background-color: var(--color-base-1);
 }
-.journal-post:hover > * {
+.tutorial-post:hover > * {
   transform: translateX(4rem);
 }
-.journal-post h1,
-.journal-post h2 {
+.tutorial-post h1,
+.tutorial-post h2 {
   margin: 0;
   padding: 0;
 }
-.journal-title {
+.tutorial-title {
   font-size: 2rem;
   color: var(--color-contrast);
 }
-.journal-excerpt {
+.tutorial-excerpt {
   color: var(--color-contrast-1);
 }
 
 @media (min-width: 560px) {
-  .journal-post {
+  .tutorial-post {
     padding: 3rem 0;
   }
 }
 
 @media (min-width: 860px) {
-  .journal-post {
+  .tutorial-post {
     padding: 5rem 0;
   }
 }
