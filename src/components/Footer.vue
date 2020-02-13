346b6f0da81a81877a2cbcc6e5853532d2207cba
Andrew Filip <andrew@afterlight.co>
2020-02-12 17:06:33 -0800

    Updated footer and wider hero

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,13 @@
 <template>
     <footer class="footer">
         <div class="container">
-            <span v-html="settings.footer" />
+            <div class="right">
+                <span v-html="settings.footer" />
+            </div>
+            <div class="footer-menu">
+                <g-link class="nav__link" to="https://instagram.com/afterlight">Instagram</g-link>
+                <g-link class="nav__link" to="https://twitter.com/afterlightapp">Twitter</g-link>
+            </div>
         </div>
     </footer>
 </template>
@@ -20,5 +26,32 @@ export default {
 .footer {
     font-size: 0.8rem;
     padding: 6rem 0;
+}
+.footer > .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.footer-menu > * {
+    font-size: 0.75rem;
+    font-weight: 600;
+    text-decoration: none;
+    margin-top: 4px;
+    margin-right: 2rem;
+    padding-bottom: 4px;
+    border-bottom: 1px solid;
+    border-color: transparent;
+    transition: border 0.15s;
+}
+.footer-menu > *:last-of-type {
+    margin: 0;
+}
+.footer-menu > *:hover {
+    border-color: inherit;
+}
+.footer-menu > .active {
+    border-color: inherit;
 }
 </style>
