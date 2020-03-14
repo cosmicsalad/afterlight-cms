83ca90555ddf0c2173576e38d79e3d9e7763799f
Andrew <andrewjfilip@gmail.com>
2020-03-13 21:46:14 -0700

    trying to fix the stupid step css heck!

--- src/pages/Tutorials.vue
+++ src/pages/Tutorials.vue
@@ -116,6 +116,44 @@ export default {
   color: var(--color-contrast-1);
 }
 
+.tutorial-step {
+  background: rgba(11,11,11,0.2);
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 50px 0;
+  border: 1px solid rgba(255,255,255,0.1);
+  border-radius: 10px;
+  padding: 2rem;
+}
+
+/* probably should remove if works */
+.tutorial-step .count {
+  background: #151515;
+  position: absolute;
+  top: -25px;
+  left: -25px;
+  font-family: Domaine;
+  font-size: 30px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  text-align: center;
+  line-height: 54px;
+}
+.tutorial-step img {
+    width: auto !important;
+  max-height: 550px !important;
+  border-radius: 5px;
+}
+.tutorial-step p {
+  text-align: left;
+  width: 100%;
+  padding: 1rem 0 0 3rem;
+}
+
 @media (min-width: 560px) {
   .tutorial-post {
     /* padding: 3rem 2rem; */
