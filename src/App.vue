9343f25d77d4fc59160f4728c2c4d34c7cab3ed1
Andrew <andrewjfilip@gmail.com>
2020-03-13 20:29:08 -0700

    Reverting away from sass due to issues for now

--- src/App.vue
+++ src/App.vue
@@ -134,29 +134,29 @@ body.dark {
   border: 1px solid rgba(255,255,255,0.1);
   border-radius: 10px;
   padding: 2rem;
-  .count {
-    background: #151515;
-    position: absolute;
-    top: -25px;
-    left: -25px;
-    font-family: Domaine;
-    font-size: 30px;
-    width: 50px;
-    height: 50px;
-    border-radius: 50px;
-    text-align: center;
-    line-height: 54px;
-  }
-  img {
-    max-height: 550px !important;
-    border-radius: 5px;
-    // box-shadow: 0 5px 10px 10px rgba(0,0,0,1.0);
-  }
-  p {
-    text-align: left;
-    width: 100%;
-    padding: 1rem 0 0 3rem;
-  }
+}
+.tutorial-step .count {
+  background: #151515;
+  position: absolute;
+  top: -25px;
+  left: -25px;
+  font-family: Domaine;
+  font-size: 30px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  text-align: center;
+  line-height: 54px;
+}
+.tutorial-step img {
+  max-height: 550px !important;
+  border-radius: 5px;
+  // box-shadow: 0 5px 10px 10px rgba(0,0,0,1.0);
+}
+.tutorial-step p {
+  text-align: left;
+  width: 100%;
+  padding: 1rem 0 0 3rem;
 }
 
 h1 {
