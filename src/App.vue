0e51a40317c53ac99d1446b69d1faf7cb10e32f6
Andrew <andrewjfilip@gmail.com>
2020-03-13 20:43:42 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -135,29 +135,6 @@ body.dark {
   border-radius: 10px;
   padding: 2rem;
 }
-.tutorial-step .count {
-  background: #151515;
-  position: absolute;
-  top: -25px;
-  left: -25px;
-  font-family: Domaine;
-  font-size: 30px;
-  width: 50px;
-  height: 50px;
-  border-radius: 50px;
-  text-align: center;
-  line-height: 54px;
-}
-.tutorial-step img {
-  max-height: 550px !important;
-  border-radius: 5px;
-  // box-shadow: 0 5px 10px 10px rgba(0,0,0,1.0);
-}
-.tutorial-step p {
-  text-align: left;
-  width: 100%;
-  padding: 1rem 0 0 3rem;
-}
 
 h1 {
   font-family: Domaine, serif;
